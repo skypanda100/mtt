997987009f32f656a03bb043ec4d39e6db3b1be8
skypanda100 <236520700@qq.com>
2018-09-23 10:35:49 +0800

    1. 数据库信息去除

--- mtt_server/routes/foodGrades.js
+++ mtt_server/routes/foodGrades.js
@@ -4,21 +4,50 @@ var util = require('../libs/util');
 var FoodGrade = require('../models/FoodGrade');
 var multer  = require('multer')
 
+const nginxPath = 'D:/nginx-1.11.13/nginx-1.11.13/html/';
+const storagePath = 'upload/';
+
 const storage = multer.diskStorage({
     destination (req, res, cb) {
-        cb(null, 'upload/');
+        cb(null, nginxPath + storagePath);
     },
     filename (req, file, cb) {
         var dateTime = util.formatDate(new Date(), 'yyyyMMddhhmmss');
-        cb(null, dateTime + file.originalname);
+        cb(null, dateTime + '-' + file.originalname);
     }
 });
 const upload = multer({storage}).any();
 
 router.put('/', upload, function (req, res, next) {
-    console.log(req);
-    res.json(null);
+    var data = req.body;
+    data.imagePath = storagePath + req.files[0].filename;
+    var foodGrade = new FoodGrade(
+        data
+    );
+    foodGrade.save((err) => {
+        if (err) {
+            next({
+                status: 500,
+                message: 'server or db error'
+            });
+        } else {
+            res.json({status: 200, message: 'save status: success'});
+        }
+    });
 });
 
+
+router.get('/', function (req, res, next) {
+    FoodGrade.find(null, null, {sort: {"dateTime": 1}}, (err, docs) => {
+        if (err) {
+            next({
+                status: 500,
+                message: 'server or db error'
+            });
+        } else {
+            res.json(docs);
+        }
+    });
+});
 
 module.exports = router;
